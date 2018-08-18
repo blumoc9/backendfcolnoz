47ce6515f3e2753d7245ef1b0d2a4ba76581ff7f
Fernando Colmenarez Oronoz <blumxp@gmail.com>
2018-08-18 01:04:18 -0300

    Se modifica documentacion para QueryDSL

--- backendfcolnoz/src/main/java/com/blumoc/backendfcolnoz/repository/QueryDSLCourseRepository.java
+++ backendfcolnoz/src/main/java/com/blumoc/backendfcolnoz/repository/QueryDSLCourseRepository.java
@@ -8,9 +8,13 @@ import com.blumoc.backendfcolnoz.entity.QCourse;
 public class QueryDSLCourseRepository {
 	
 	/**
-	 * Para usar los query dsl vamos a necesitar las clases Q
-	 * para este ejemplo vamos a usar la Clase Course y para que sea clase de tipo Course
-	 * debemos agregarle una Q al principio, es decir : QCourse y generara en la carpeta target la clase q course
+	 * Para usar los query dsl vamos a necesitar las  Q-Clases
+	 * para este ejemplo vamos a usar la Clase Course y para que sea clase de tipo QCourse
+	 * debemos agregarle una Q al principio, es decir : QCourse y  la misma
+	 * se generara en la carpeta target de manera automatica, pero paso previo debemos tener las
+	 * dependencias que se indican en la siguiente pagina
+	 * 
+	 * <a>http://www.querydsl.com/static/querydsl/4.1.3/reference/html_single/</a>
 	 * automatico, en caso contrario, debemos abril la terminal y hacer lo siquie
 	 */
 	private QCourse qCourse = QCourse.course;
